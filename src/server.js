9e9f68f8a8fbe22b8aa8e168ac6f0f30d9026093
Elrey Belmonti <elrey.belmonti@formidable.com>
2019-09-26 09:23:54 -0700

    expres routes, middlware, and sub routes

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,36 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const log = (request, response, next) => {
+  console.log('logging')
+  request.mydata = 'hello'
+  next()
+}
+
+/**
+ * C - post
+ * R - get
+ * U - put
+ * D - delete
+ */
+
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+app.use('/api', router)
+
+app.get('/data', (request, response) => {
+  response.send({ data: request.mydata })
+})
+
+app.post('/data', (request, response) => {
+  console.log(request.body)
+  response.send({ ok: true })
+})
+
+export const start = () => {
+  app.listen(8080, () => {
+    console.log('Server is on 8080')
+  })
+}
