7be93c10a12e30b48c8572f0fcce666329c64cb0
Elrey Belmonti <elrey.belmonti@formidable.com>
2019-09-26 10:15:10 -0700

    router and sub router exercise

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,20 @@
 import { Router } from 'express'
 
+const controller = (req, res) => {
+  res.send({ mesage: 'hello' })
+}
+
 const router = Router()
+
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
