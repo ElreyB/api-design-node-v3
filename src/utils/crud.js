1e48dc6e34cea3741551881b2c030a67d225b67c
Elrey Belmonti <elrey.belmonti@formidable.com>
2019-09-27 07:32:10 -0700

    create general controller

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,65 @@
-export const getOne = model => async (req, res) => {}
+import mongoose from 'mongoose'
 
-export const getMany = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  console.log(req.params._id, req.params.user)
+  const id = req.params.id
+  const user = req.user._id
+  const doc = await model.findOne({ _id: id, createdBy: user })
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
 
-export const createOne = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const user = req.user._id
+  const docs = await model.find({ createdBy: user })
+  if (!docs) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: docs })
+}
 
-export const updateOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  const user = req.user._id
+  const name = req.body.name
+  const newDoc = await model.create({
+    name,
+    createdBy: user,
+    list: mongoose.Types.ObjectId()
+  })
+  if (!user) {
+    return res.status(400).end()
+  }
+  res.status(201).json({ data: newDoc })
+}
 
-export const removeOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  const user = req.user._id
+  const listId = req.params.id
+  const update = req.body
+  const doc = await model.findOneAndUpdate(
+    { _id: listId, createdBy: user },
+    update,
+    { new: true }
+  )
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
+
+export const removeOne = model => async (req, res) => {
+  const user = req.user._id
+  const listId = req.params.id
+  const doc = await model
+    .findOneAndRemove({ _id: listId, createdBy: user })
+    .exec()
+  if (!doc) {
+    return res.status(400).end()
+  }
+  res.status(200).json({ data: doc })
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
